dd070203519c41466944bb9537e8694c61b036a5
yaocz <yaocz@tesr.com>
2022-03-23 11:46:35 +0800

    修改注释

--- Easyauto/testdir/testdemo.py
+++ Easyauto/testdir/testdemo.py
@@ -5,7 +5,7 @@ sys.path.append(path)
 import Easyauto
 
 class SampleTest(Easyauto.TestCase):
-
+    # 这是分支提交 2022年3月23日11:45:52
     def test_case(self):
         """a simple test case """
         self.open("http://www.itest.info")
