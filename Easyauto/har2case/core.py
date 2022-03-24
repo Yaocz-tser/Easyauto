264a9541f8df3a4e94fc3543de5e85a7f9856ae9
yaocz <yaocz@tesr.com>
2022-03-24 16:07:28 +0800

    update 2022年3月24日16:07:24

---
+++ Easyauto/har2case/core.py
@@ -0,0 +1,112 @@
+import os
+from Easyauto.logging import log
+from Easyauto.har2case import utils
+
+
+class HarParser(object):
+
+    def __init__(self, har_file_path):
+        self.har_file_path = har_file_path
+        self.case_template = """import Easyauto
+
+
+class TestRequest(Easyauto.TestCase):
+
+    def start(self):
+        self.url = "{url}"
+
+    def test_case(self):
+        headers = {header}
+        cookies = {cookie}
+        self.{method}(self.url, {params}, headers=headers, cookies=cookies)
+        self.assertStatusCode({resp_status})
+
+
+if __name__ == '__main__':
+    Easyauto.main()
+"""
+
+    def _make_testcase(self):
+        """
+        make test case.
+        test case are parsed from HAR log entries list.
+        """
+        testcase = ""
+        log_entries = utils.load_har_log_entries(self.har_file_path)
+
+        for entry_json in log_entries:
+            url = entry_json["request"].get("url")
+            method = entry_json["request"].get("method").lower()
+            headers = entry_json["request"].get("headers")
+            cookies = entry_json["request"].get("cookies")
+
+            response_status = entry_json["response"].get("status")
+
+            headers_str = utils.list_to_dict_str(headers)
+            cookies_str = utils.list_to_dict_str(cookies)
+            if "?" in url:
+                url = url.split("?")[0]
+
+            data_str = ""
+            if method == "post" or method == "put" or method == "delete":
+                # from-data
+                params = entry_json["request"]["postData"].get("params")
+                if params is not None:
+                    params_dict = utils.list_to_dict_str(params)
+                    data_str = "data=" + params_dict
+                else:
+                    data_str = "data={}"
+                # json
+                text = entry_json["request"]["postData"].get("text")
+                mime_type = entry_json["request"]["postData"].get("mimeType")
+                if mime_type is not None:
+                    if mime_type == "application/json":
+                        data_str = "json=" + text
+                else:
+                    data_str = "json={}"
+
+            elif method == "get":
+                # params
+                query_string = entry_json["request"].get("queryString")
+                if query_string is not None:
+                    query_string_str = utils.list_to_dict_str(query_string)
+                    data_str = "params=" + query_string_str
+                else:
+                    data_str = "params={}"
+            else:
+                raise TypeError("Only POST/GET/PUT/DELETE methods are supported。")
+
+            testcase = self.case_template.format(header=headers_str,
+                                                 cookie=cookies_str,
+                                                 method=method,
+                                                 url=url,
+                                                 params=data_str,
+                                                 resp_status=response_status)
+
+        return testcase
+
+    def create_file(self, save_path, file_content=""):
+        """
+        create test case file
+        """
+        with open(save_path, 'w') as f:
+            f.write(file_content)
+        msg = "created file: {}".format(save_path)
+        log.info(msg)
+
+    def gen_testcase(self):
+        har_file = os.path.splitext(self.har_file_path)[0]
+        output_testcase_file = "{}.py".format(har_file)
+        print(output_testcase_file)
+
+        log.info("Start to generate testcase.")
+        testcase = self._make_testcase()
+
+        har_path = os.path.dirname(os.path.abspath(har_file))
+        self.create_file(os.path.join(har_path, output_testcase_file), testcase)
+
+
+if __name__ == '__main__':
+    hp = HarParser("./demo.har")
+    hp.gen_testcase()
+
---
+++ Easyauto/har2case/utils.py
@@ -0,0 +1,48 @@
+import io
+import sys
+import json
+import logging
+
+
+def load_har_log_entries(file_path):
+    """ load HAR file and return log entries list
+
+    Args:
+        file_path (str)
+
+    Returns:
+        list: entries
+            [
+                {
+                    "request": {},
+                    "response": {}
+                },
+                {
+                    "request": {},
+                    "response": {}
+                }
+            ]
+
+    """
+    with io.open(file_path, "r+", encoding="utf-8-sig") as f:
+        try:
+            content_json = json.loads(f.read())
+            return content_json["log"]["entries"]
+        except (KeyError, TypeError):
+            logging.error("HAR file content error: {}".format(file_path))
+            sys.exit(1)
+
+
+def list_to_dict_str(data: list):
+    """
+    list -> dict -> string
+    """
+    data_dict = {}
+    for param in data:
+        data_dict[param["name"]] = param["value"]
+
+    if len(data_dict) == 0:
+        data_dict_str = "{}"
+    else:
+        data_dict_str = json.dumps(data_dict)
+    return data_dict_str
